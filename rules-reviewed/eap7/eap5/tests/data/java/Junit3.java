bb9d71a5f64a3efc3cda6b5be142cdbaaa4cec05
Brad Flood <bflood@keyholesoftware.com>
2017-09-04 13:30:37 -0500

    initial commit of custom ruleset project, modified from windup-rulesets.

---
+++ rules-reviewed/eap7/eap5/tests/data/java/Junit3.java
@@ -0,0 +1,25 @@
+
+// Contains code that should match for the following tests:
+//
+// bwf-00000-test
+//
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import junit.framework.TestCase;
+
+public class RegularExpressionTest extends TestCase {
+
+	private String zipRegEx = "^\\d{5}([\\-]\\d{4})?$";
+	private Pattern pattern;
+
+	protected void setUp() throws Exception {
+		this.pattern = Pattern.compile(this.zipRegEx);
+	}
+
+	public void testZipCode() throws Exception {
+		Matcher mtcher = this.pattern.matcher("22101");
+		boolean isValid = mtcher.matches();
+		assertTrue("Pattern did not validate zip code", isValid);
+	}
+}
